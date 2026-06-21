1ab8c4098660804c83df4a78eee4144707a5b52e
yarraguntlaspandana <yarraguntlaspandana@gmail.com>
2026-06-21 20:55:27 +0530

    Added deployment port

--- app.js
+++ app.js
@@ -7,40 +7,42 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 const jobs = JSON.parse(
-  fs.readFileSync("./data/jobs.json")
+    fs.readFileSync("./data/jobs.json")
 );
 
 app.get("/", (req, res) => {
-  res.render("home");
+    res.render("home");
 });
 
 app.get("/dashboard", (req, res) => {
 
-  let skills = {};
-  let locations = {};
-  let experience = {};
+    let skills = {};
+    let locations = {};
+    let experience = {};
 
-  jobs.forEach(job => {
+    jobs.forEach(job => {
 
-    job.skills.forEach(skill => {
-      skills[skill] = (skills[skill] || 0) + 1;
+        job.skills.forEach(skill => {
+            skills[skill] = (skills[skill] || 0) + 1;
+        });
+
+        locations[job.location] =
+            (locations[job.location] || 0) + 1;
+
+        experience[job.experience] =
+            (experience[job.experience] || 0) + 1;
     });
 
-    locations[job.location] =
-      (locations[job.location] || 0) + 1;
-
-    experience[job.experience] =
-      (experience[job.experience] || 0) + 1;
-  });
-
-  res.render("dashboard", {
-    skills,
-    locations,
-    experience
-  });
+    res.render("dashboard", {
+        skills,
+        locations,
+        experience
+    });
 
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
